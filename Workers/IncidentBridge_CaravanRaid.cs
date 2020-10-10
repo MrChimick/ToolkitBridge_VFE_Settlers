845529bfa2bf5b98ae92e2fc37bfb30fc9d5b177
Mr_Chimick <mikechimick@gmail.com>
2020-10-09 20:41:26 -0700

    Base mod completed, only for Rimworld v1.2 currently

---
+++ Workers/IncidentBridge_CaravanRaid.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+using TwitchToolkit.Store;
+
+namespace ToolkitBridge_VFE_Settlers
+{
+    public class IncidentBridge_CaravanRaid : IncidentHelper
+    {
+        private IncidentParms parms;
+        private IncidentWorker worker;
+        
+
+        public override bool IsPossible()
+        {
+            this.worker = (IncidentWorker) new IncidentWorker_GiveQuest();
+            this.worker.def = IncidentDef.Named("GiveQuest_CaravanRaid");
+            this.worker.def.questScriptDef = DefDatabase<QuestScriptDef>.GetNamed("VFES_CaravanRaid");
+            this.parms = new IncidentParms();
+            this.parms.questScriptDef = DefDatabase<QuestScriptDef>.GetNamed("VFES_CaravanRaid");
+            this.parms.points = StorytellerUtility.DefaultThreatPointsNow(Current.Game.RandomPlayerHomeMap);
+            this.parms.forced = true;
+            return (this.worker.CanFireNow(this.parms, false));
+        }
+
+        public override void TryExecute() => this.worker.TryExecute(this.parms);
+    }
+}
---
+++ Workers/IncidentBridge_Wanted.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+using TwitchToolkit.Store;
+
+namespace ToolkitBridge_VFE_Settlers
+{
+    public class IncidentBridge_Wanted : IncidentHelper
+    {
+        private IncidentParms parms;
+        private IncidentWorker worker;
+        
+
+        public override bool IsPossible()
+        {
+            this.worker = (IncidentWorker) new IncidentWorker_GiveQuest();
+            this.worker.def = IncidentDef.Named("GiveQuest_Settlers_Wanted");
+            this.worker.def.questScriptDef = DefDatabase<QuestScriptDef>.GetNamed("Settlers_Wanted");
+            this.parms = new IncidentParms();
+            this.parms.questScriptDef = DefDatabase<QuestScriptDef>.GetNamed("Settlers_Wanted");
+            this.parms.points = StorytellerUtility.DefaultThreatPointsNow(Current.Game.RandomPlayerHomeMap);
+            this.parms.forced = true;
+            return (this.worker.CanFireNow(this.parms, false));
+        }
+
+        public override void TryExecute() => this.worker.TryExecute(this.parms);
+    }
+}
